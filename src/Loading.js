8858de6181e752e2ff2835768534e514a1a0a59f
sheik salman <gsheiksalman7@gmail.com>
2025-10-17 17:18:07 +0530

    made loader visible in both screens

--- src/Loading.js
+++ src/Loading.js
@@ -2,14 +2,25 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center bg-black bg-opacity-90 m-4 h-screen">
+    <div className="flex justify-center z-50 items-center bg-black bg-opacity-90 m-4 min-h-screen">
       <div className="flex flex-col items-center space-y-4">
         <div className="flex space-x-2">
-          <div className="w-3 h-3 bg-red-600 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-          <div className="w-3 h-3 bg-red-600 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-          <div className="w-3 h-3 bg-red-600 rounded-full animate-bounce"></div>
+          <div
+            className="w-3 h-3 bg-red-600 rounded-full animate-bounce"
+            style={{ animationDelay: '0.3s' }}
+          ></div>
+          <div
+            className="w-3 h-3 bg-red-600 rounded-full animate-bounce"
+            style={{ animationDelay: '0.15s' }}
+          ></div>
+          <div
+            className="w-3 h-3 bg-red-600 rounded-full animate-bounce"
+            style={{ animationDelay: '0s' }}
+          ></div>
         </div>
-        <p className="text-white text-sm md:text-base font-medium">Great Pick! Fetching movies...</p>
+        <p className="text-white text-sm md:text-base font-medium">
+          Great Pick! Fetching movies...
+        </p>
       </div>
     </div>
   )
